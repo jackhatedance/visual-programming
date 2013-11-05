69f15e122063f3e1ef66fae970f3928420de3c30
jackding <jackding@cisco.com>
2013-11-05 09:17:50 +0800

    code clean

--- src/test/java/com/bluesky/visualprogramming/vm/AppPropertiesTest.java
+++ src/test/java/com/bluesky/visualprogramming/vm/AppPropertiesTest.java
@@ -1,10 +1,12 @@
 package com.bluesky.visualprogramming.vm;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class AppPropertiesTest {
 
+	@Ignore
 	@Test
 	public void test() {
 		String value = AppProperties.getInstance().getRemoteSecurityConfig()
