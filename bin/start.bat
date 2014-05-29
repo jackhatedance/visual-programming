7d88b1a475d5558c21a9938976a13b482c63fe14
Jack Ding <jackhatedance@gmail.com>
2014-05-30 00:30:30 +0800

    fix bin script

--- bin/start.bat
+++ bin/start.bat
@@ -3,7 +3,9 @@ set home_dir=%~dp0%..
 
 cd "%home_dir%"
 
-for %%i in (cooby-?.?.jar) do set jar=%%i
+remm for %%i in (cooby-?.?.jar) do set jar=%%i
+rem echo %jar%
 
-echo %jar%
-java -jar %home_dir%\%jar% 
+rem the conf classpath is to overwrite default configurations
+java -cp conf;* com.bluesky.visualprogramming.ui.Main
+
--- bin/start.sh
+++ bin/start.sh
@@ -7,7 +7,9 @@ cd ..
 
 BASE_DIR="$( pwd )"
 echo base dir:$BASE_DIR
-JAR=$(ls lib/cooby*.jar)
-echo $JAR
-java -jar $JAR
+#JAR=$(ls lib/cooby*.jar)
+#echo $JAR
+
+#the conf classpath is to overwrite default configurations
+java -cp $BASE_DIR/conf:* com.bluesky.visualprogramming.ui.Main
 
